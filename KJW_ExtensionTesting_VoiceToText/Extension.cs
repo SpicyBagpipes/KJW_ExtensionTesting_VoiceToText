2d873ca54d23c23c665523a0db7b0cad2517d354
KJW <ppbtgr@gmail.com>
2023-04-01 17:01:01 +0100

    sync with vc

--- KJW_ExtensionTesting_VoiceToText/Extension.cs
+++ KJW_ExtensionTesting_VoiceToText/Extension.cs
@@ -20,10 +20,10 @@ namespace KJW_ExtensionTesting_VoiceToText
 		#else
 			[DllExport("_RVExtensionRegisterCallback@4", CallingConvention = CallingConvention.Winapi)]
 		#endif
-	        public static void RVExtensionRegisterCallback([MarshalAs(UnmanagedType.FunctionPtr)] ExtensionCallback func)
-			{
-	            callback = func;
-		    }
+		public static void RVExtensionRegisterCallback([MarshalAs(UnmanagedType.FunctionPtr)] ExtensionCallback func)
+		{
+			callback = func;
+		}
 
         /// <summary>
         /// Gets called when arma starts up and loads all extension.
@@ -47,15 +47,16 @@ namespace KJW_ExtensionTesting_VoiceToText
         /// <param name="output">The string builder object that contains the result of the function</param>
         /// <param name="outputSize">The maximum size of bytes that can be returned</param>
         /// <param name="function">The string argument that is used along with callExtension</param>
-#if WIN64
+		#if WIN64
 			[DllExport("RVExtension", CallingConvention = CallingConvention.Winapi)]
-#else
-        [DllExport("_RVExtension@12", CallingConvention = CallingConvention.Winapi)]
+		#else
+	        [DllExport("_RVExtension@12", CallingConvention = CallingConvention.Winapi)]
 		#endif
-			public static void RvExtension(StringBuilder output, int outputSize,
-			   [MarshalAs(UnmanagedType.LPStr)] string function) {
-					Worker.EngineHandler(function);
-				}
+		public static void RvExtension(StringBuilder output, int outputSize,
+		[MarshalAs(UnmanagedType.LPStr)] string function) {
+			Worker.EngineHandler(function);
+			callback("KJW_Debug", "Hello", "World");
+		}
 
 		/// <summary>
 		/// The entry point for the callExtensionArgs command.
@@ -71,20 +72,12 @@ namespace KJW_ExtensionTesting_VoiceToText
 		#else
 			[DllExport("_RVExtensionArgs@20", CallingConvention = CallingConvention.Winapi)]
 		#endif
-			public static int RvExtensionArgs(StringBuilder output, int outputSize,
-				[MarshalAs(UnmanagedType.LPStr)] string function,
-				[MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.LPStr, SizeParamIndex = 4)] string[] args, int argCount) {
-					//Function goes here.
-					return 0;
-				}
-
-        // Callback.
-        /*
-			callback ("Name", "Function", "Data").
-			Name: KJW_Core_VoiceRecognised
-			Function: make it name of the function the extension sends the result to. (Note: The returned function is just a STRING! So compile is needed, before using call or spawn, to execute it).
-			Data: params returned. 
-			All of these are just strings passed as params to the MEH.
-		*/
+		public static int RvExtensionArgs(StringBuilder output, int outputSize,
+		[MarshalAs(UnmanagedType.LPStr)] string function,
+		[MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.LPStr, SizeParamIndex = 4)] string[] args, int argCount) {
+			//Function goes here.
+			callback("KJW_Debug", "Fuck", "you");
+			return 0;
+		}
     }
 }
